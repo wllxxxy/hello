d1f9003a2db78f92db9834140f64f338f4989974
wangliangliang <wangliangliang@yuanxin-inc.com>
2020-03-30 22:03:22 +0800

    hello组件

---
+++ src/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wll
+ * Date: 2020/3/30
+ * Time: 21:36
+ */
+namespace wllxxxy\hello;
+class SayHello
+{
+    public function world()
+    {
+        echo 'hello world';
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wll
+ * Date: 2020/3/30
+ * Time: 22:00
+ */
+require '../vendor/autoload.php';
+
+$sayHello = new \wllxxxy\hello\SayHello();
+$sayHello->world();
